84e1689664c4d16f6c417a479672113e3054f4db
Matt Scilipoti <matt@scilipoti.name>
2009-10-07 16:03:07 -0400

    Replaced `require` in spec_helper to ensure lib files are loaded instead of gem (quite strange).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
 require 'rubygems'
 require 'spec'
-require File.dirname(__FILE__) + '/../lib/nestegg'
+
+#Replaced this `require` with `$LOAD_PATH.unshift`
+#to ensure the file is used instead of the gem.
+#lib/nestegg.rb was loading nestegg/version from the gem
+#instead of lib/nestegg/version.rb
+#require File.dirname(__FILE__) + '/../lib/nestegg'
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../lib'))
+require 'nestegg'
